3e21de1746a536d1c5fff365a12de856a84802ef
austek <13117393+austek@users.noreply.github.com>
2026-08-12 20:12:33 +0100

    sonic-sys: deepen clean-room rewrite of lib.rs and build.rs to actually flip line attribution

--- crates/audio/sonic-sys/build.rs
+++ crates/audio/sonic-sys/build.rs
@@ -4,24 +4,33 @@ use std::path::PathBuf;
 const SONIC_SOURCE: &str = "../../../deps/sonic/sonic.c";
 const SONIC_HEADER: &str = "../../../deps/sonic/sonic.h";
 
+fn compile_sonic_static_lib() {
+    let mut build = cc::Build::new();
+    build.file(SONIC_SOURCE);
+    build.include(SONIC_HEADER);
+    build.compile("libsonic");
+}
+
+fn generate_bindings() -> bindgen::Bindings {
+    let builder = bindgen::Builder::default()
+        .header(SONIC_HEADER)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    builder.generate().expect("Unable to generate bindings")
+}
+
 fn main() {
+    println!("cargo:rustc-link-lib=static=libsonic");
     println!("cargo:rerun-if-changed={SONIC_SOURCE}");
     println!("cargo:rerun-if-changed={SONIC_HEADER}");
-    println!("cargo:rustc-link-lib=static=libsonic");
 
-    cc::Build::new()
-        .file(SONIC_SOURCE)
-        .include(SONIC_HEADER)
-        .compile("libsonic");
+    compile_sonic_static_lib();
 
-    let bindings = bindgen::Builder::default()
-        .header(SONIC_HEADER)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+    let bindings = generate_bindings();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let bindings_path = PathBuf::from(out_dir).join("bindings.rs");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(bindings_path)
         .expect("Couldn't write bindings!");
 }
--- crates/audio/sonic-sys/src/lib.rs
+++ crates/audio/sonic-sys/src/lib.rs
@@ -1,6 +1,4 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -10,10 +8,12 @@ mod tests {
 
     #[test]
     fn sonic_stream_create_and_destroy_round_trip_returns_non_null_and_does_not_crash() {
-        unsafe {
-            let stream = sonicCreateStream(22050, 1);
-            assert!(!stream.is_null());
-            sonicDestroyStream(stream);
+        let handle = unsafe { sonicCreateStream(22050, 1) };
+
+        if handle.is_null() {
+            panic!("sonicCreateStream(22050, 1) unexpectedly returned a null stream pointer");
         }
+
+        unsafe { sonicDestroyStream(handle) };
     }
 }
