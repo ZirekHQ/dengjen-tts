fd7b2b4fc38718cea2aff9479794f749223f17de
austek <13117393+austek@users.noreply.github.com>
2026-08-12 20:08:50 +0100

    sonic-sys: clean-room rewrite of build.rs and lib.rs

--- crates/audio/sonic-sys/build.rs
+++ crates/audio/sonic-sys/build.rs
@@ -1,23 +1,25 @@
 use std::env;
 use std::path::PathBuf;
 
+const SONIC_SOURCE: &str = "../../../deps/sonic/sonic.c";
+const SONIC_HEADER: &str = "../../../deps/sonic/sonic.h";
+
 fn main() {
+    println!("cargo:rerun-if-changed={SONIC_SOURCE}");
+    println!("cargo:rerun-if-changed={SONIC_HEADER}");
     println!("cargo:rustc-link-lib=static=libsonic");
-    println!("cargo:rerun-if-changed=../../../deps/sonic/sonic.h");
-    println!("cargo:rerun-if-changed=../../../deps/sonic/sonic.c");
 
     cc::Build::new()
-        .file("../../../deps/sonic/sonic.c")
-        .include("../../../deps/sonic/sonic.h")
+        .file(SONIC_SOURCE)
+        .include(SONIC_HEADER)
         .compile("libsonic");
 
     let bindings = bindgen::Builder::default()
-        .header("../../../deps/sonic/sonic.h")
+        .header(SONIC_HEADER)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
